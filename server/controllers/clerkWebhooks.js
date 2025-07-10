c42fe91f768e31d65a61cb3f6ad96fcf0a5ecd0c
SanjayHordak <sanjay604116@gmail.com>
2025-07-10 19:32:46 +0530

    updated

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -48,9 +48,9 @@ const clerkWebhooks=async(req,res)=>{
          }
          res.json({success:true, message:"Webhook Received"})
 
-    }catch(err){
-        console.log(err.message)
-        res.json({success:false, message:err.message})
+    }catch(error){
+        console.log(error.message)
+        res.json({success:false, message:error.message})
     }
 }
 
